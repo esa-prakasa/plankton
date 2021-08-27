c08bc8f4744787175381cef0a83146fcf70415c0
Esa Prakasa <esa.prakasa@gmail.com>
2021-08-27 15:18:22 +0700

    Add files via upload

---
+++ overlay.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+import numpy as np
+
+
+#rgbPath = r"C:\Users\Esa\Pictures\_DATASET\Plank30\kFoldFolders\D3\Thalassiosira sp (large) (400X) PariIslandRisetPro19 St10_FDpXsbOPwA\images\Thalassiosira sp (large) (400X) PariIslandRisetPro19 St10_FDpXsbOPwA.jpg"
+#bwPath = r"C:\Users\Esa\Pictures\_DATASET\Plank30\result0\Thalassiosira sp (large) (400X) PariIslandRisetPro19 St10_FDpXsbOPwA.jpg"
+
+
+rgbPath = r"C:\Users\Esa\Pictures\_DATASET\Plank30\kFoldFolders\D3\Chaetoceros laciniosus (200X) PariIslandRisetPro2019 St10\images\Chaetoceros laciniosus (200X) PariIslandRisetPro2019 St10.jpg"
+bwPath = r"C:\Users\Esa\Pictures\_DATASET\Plank30\result0\Chaetoceros laciniosus (200X) PariIslandRisetPro2019 St10.jpg"
+
+
+
+rgb = cv2.imread(rgbPath)
+bw = cv2.imread(bwPath)
+
+M = bw.shape[0]
+N = bw.shape[1]
+
+seg = np.zeros((M, N, 3), np.uint8)
+
+for i in range(M):
+    for j in range(N):
+        if bw[i,j,0] == 255:
+            for k in range(3):
+                seg[i,j,k] = rgb[i,j,k]
+
+#cv2.imshow("rgb", rgb)
+#cv2.imshow("bw", bw)
+cv2.imshow("SEG", seg)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ readJsonPlankASingle.py
@@ -0,0 +1,40 @@
+import numpy as np 
+import json
+import os
+import cv2
+
+os.system("cls")
+
+jsonPath = r"C:\Users\Esa\Documents\___KEGIATAN-Ku 2021\01.21 -- Plankto IPK\IPB Conf 2021\Figures\Thalassionema nitzschioides-2 (200X) PariIslandRisetPro2019 St10.jpg.json"
+rgbPath = r"C:\Users\Esa\Documents\___KEGIATAN-Ku 2021\01.21 -- Plankto IPK\IPB Conf 2021\Figures\PlanktonSegmentation_Plank30_Thalassionema nitzschioides-2 (200X) PariIslandRisetPro2019 St10.jpg"
+
+print(jsonPath)
+print(rgbPath)
+
+img0 = cv2.imread(rgbPath)
+M = img0.shape[0]
+N = img0.shape[1]
+
+with open(jsonPath) as json_file:
+	data = json.load(json_file)
+
+NoOfObj = len(data['objects'])
+print(NoOfObj)
+
+bwImg = np.zeros((M, N, 3), np.uint8)
+
+pcs = data['objects'][0]['points']['exterior']
+print(pcs)
+    
+polygon = np.array(pcs).astype('int32')
+cv2.fillPoly(bwImg, pts = [polygon], color =(255,255,255))
+
+cv2.imshow("RGB",img0)
+cv2.imshow("BW",bwImg)
+
+#cv2.imwrite((os.path.join(bwPath,jF[:-5])),bwImg)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
